11569eb609ee822ee7996cd5bcb17dd64a2ebed0
Roy Smith <roy.smith.esq@gmail.com>
2024-09-13 17:54:21 +0000

    add import_finder

--- plugin/nvim1.lua
+++ plugin/nvim1.lua
@@ -1,5 +1,7 @@
   -- plugin/hello_world.lua
 
 local hello = require("nvim1")
-
 vim.api.nvim_create_user_command("HelloWorld", hello.say_hello, {})
+
+local find_imports = require("find_imports")
+vim.api.nvim_create_user_command("FindImports", find_imports.find_imports, { range = true, nargs = 0 })
