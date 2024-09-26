f124df93e165b18cd93ccb745eb8a24618e0a59d
Moonlit Knight <roy.smith.esq+gh@gmail.com>
2024-09-26 16:01:59 +0200

    Update tsedit.lua
    TsStartEdit, TsEndEdit

--- plugin/tsedit.lua
+++ plugin/tsedit.lua
@@ -1,1 +1,3 @@
 local tsedit = require("tsedit")
+vim.api.nvim_create_user_command("TsStartEdit", tsedit.ts_start_edit, { range = true, nargs = 0 })
+vim.api.nvim_create_user_command("TsEndEdit", tsedit.ts_end_edit, { range = true, nargs = 0 })
