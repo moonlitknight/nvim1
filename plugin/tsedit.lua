034c122723c6fb1c087e5fec7fd4528c22ebe8ef
Moonlit Knight <roy.smith.esq+gh@gmail.com>
2024-09-26 16:21:10 +0200

    Update tsedit.lua keymaps

--- plugin/tsedit.lua
+++ plugin/tsedit.lua
@@ -1,3 +1,6 @@
 local tsedit = require("tsedit")
 vim.api.nvim_create_user_command("TsStartEdit", tsedit.ts_start_edit, { range = true, nargs = 0 })
 vim.api.nvim_create_user_command("TsEndEdit", tsedit.ts_end_edit, { range = true, nargs = 0 })
+vim.api.nvim_set_keymap('n', '<leader>.ts', ':TsStartEdit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>.te', ':TsEndEdit<CR>', { noremap = true, silent = true })
+
